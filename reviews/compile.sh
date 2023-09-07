1ef2a5077690e963c4fe1d243dc418f88f258ab0
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-09-07 11:09:20 -0400

    ready

--- reviews/compile.sh
+++ reviews/compile.sh
@@ -1,1 +1,2 @@
-find . -type f -name "*.md" -exec bash -c 'pandoc {} -o {}.pdf --template=../.assets/templates/responses.tex' \;
+#find . -type f -name "*.md" -exec bash -c 'pandoc {} -o {}.pdf --template=../.assets/templates/responses.tex' \;
+find . -type f -name "*.md" -exec bash -c 'pandoc {} -o {}.pdf' \;
